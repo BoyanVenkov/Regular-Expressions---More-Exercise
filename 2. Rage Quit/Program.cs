5223d4151251b86e699ce65a6112d028b0a06b62
Boyan Venkov <boyanvenkov@yahoo.com>
2026-02-06 17:17:37 +0200

    Added a New Project

---
+++ 2. Rage Quit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+StringBuilder result = new StringBuilder();
+HashSet<char> uniqueSymbols = new HashSet<char>();
+
+MatchCollection matches = Regex.Matches(input, @"([^\d]+)(\d+)");
+
+foreach (Match match in matches)
+{
+    string text = match.Groups[1].Value.ToUpper();
+    int count = int.Parse(match.Groups[2].Value);
+
+    for (int i = 0; i < count; i++)
+    {
+        result.Append(text);
+    }
+}
+
+foreach (char ch in result.ToString())
+{
+    uniqueSymbols.Add(ch);
+}
+
+Console.WriteLine($"Unique symbols used: {uniqueSymbols.Count}");
+Console.WriteLine(result.ToString());
